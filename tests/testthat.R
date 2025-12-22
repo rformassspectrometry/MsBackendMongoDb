1c6c4ae76a763e746a19e380b4aff77c2b8b1576
Johannes Rainer <johannes.rainer@gmail.com>
2025-12-22 11:02:29 +0100

    Clean up unit tests

--- tests/testthat.R
+++ tests/testthat.R
@@ -3,14 +3,9 @@ library(testthat)
 library(MsBackendMongoDb)
 library(mongolite)
 library(Spectra)
-library(msdata)
 
 #' [TODO] we can run the unit tests only if a mongodb server is running on the
 #' system. We should check how to best tackle that. Maybe looking at the unit
 #' tests of the mongolite package?
 
 test_check("MsBackendMongoDb")
-
-#' TODO @jo
-#'
-#' - [ ] continue unit test check from [ on.
